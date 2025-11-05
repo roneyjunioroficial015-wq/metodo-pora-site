8f44dd221038a543de33fc5ca5d5cd8d08575f44
roneyjunioroficial015-wq <roneyjunioroficial015@gmail.com>
2025-11-05 01:59:42 +0000

    atualizando site metodo pora

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+      const botao = document.querySelector(".botao-comprar");
+        botao.addEventListener("click", () => {
+            alert("Você será redirecionado para a página de pagamento da Kiwify!");
+              });
+              });
+})
